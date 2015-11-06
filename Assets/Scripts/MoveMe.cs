cc5bf7a682097d2d0812ecf2ef5e6ce65f6535a0
Scub3d <scubedtechnologiesllc@gmail.com>
2015-11-06 18:59:02 -0500

    pushing

---
+++ Assets/Scripts/MoveMe.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveMe : MonoBehaviour {
+
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+		GameObject bullet = (GameObject)Instantiate(prefab, new Vector3(0,0,0), Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/bulletController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().velocity = new Vector3 (0, 5f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
